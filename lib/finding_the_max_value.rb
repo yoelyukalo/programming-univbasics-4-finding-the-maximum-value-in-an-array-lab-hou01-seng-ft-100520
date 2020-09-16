5742c44b24953e4d7beae192d96a0bb1ec39a410
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-16 18:21:27 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,12 @@
 def find_max_value(array)
   count = 0 
-  found_num = nil 
+  max_value = -1
   while count < array.lenght do
     puts array[count]
-    if array[count] < array[count - 1]
-      puts found_num
+    if max_value < array[count]
+      max_value = array[count]
     end
     count += 1
   end
+  max_value
 end
